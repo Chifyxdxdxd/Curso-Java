12b3d92175460c2a8e6eb356ac3c3eec1d8bfdc0
Chifyxdxdxd <114033327+Chifyxdxdxd@users.noreply.github.com>
2022-10-03 22:52:33 -0500

    Adding s-5
    Creando archivos ejercicios sobre matrices

---
+++ Matrices/MatrizMarco1/src/main/java/com/mycompany/matrizmarco1/MatrizMarco1.java
@@ -0,0 +1,16 @@
+package com.mycompany.matrizmarco1;
+
+import java.util.Scanner;
+
+/**
+ * Crear y cargar una matriz de tamaño n x m y decir si es simétrica o no.
+ */
+public class MatrizMarco1 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int mat[][], fil, col;
+        
+        sout
+    }
+}
